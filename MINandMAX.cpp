75d5dba7f617aa554dfb19c41f476066fbc0408c
Ranjay6126 <6126ranjayprajapati@gmail.com>
2026-04-19 18:58:45 +0530

    min and max

---
+++ MINandMAX.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+#include <climits>
+using namespace std;
+
+int Max(int barray[], int size){
+    int max = INT_MIN;
+    
+    for(int i=0; i<size; i++){
+        if(barray[i] > max){
+            max = barray[i];
+        }
+    }
+    return max;
+}
+
+int Min(int barray[], int size){
+    int min = INT_MAX;
+    for(int i=0; i<size; i++){
+        if(barray[i]<min){
+            min = barray[i];
+        }
+    }
+    return min;
+}
+
+int main(){
+    
+    int barray[100];
+    int size ;
+    cout<<"take the input of size"<<endl;
+    cin>>size;
+    
+    for(int i=0; i<size; i++){
+        cin>>barray[i];
+    }
+    
+    
+    cout<<"the max value is:" << Max(barray, size)<<endl;
+    cout<<"the min value is: " <<  Min(barray, size)<<endl;
+    
+}

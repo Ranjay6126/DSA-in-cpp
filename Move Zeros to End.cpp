413583c4e6e367e0289a659704f971a1749eb728
Ranjay6126 <6126ranjayprajapati@gmail.com>
2026-04-19 23:49:26 +0530

    move zero to end

---
+++ Move Zeros to End.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+void moveZeros(int arr[], int n){
+    int j = 0;
+
+    for(int i=0; i<n; i++){
+        if(arr[i] != 0){
+            swap(arr[i], arr[j]);
+            j++;
+        }
+    }
+}

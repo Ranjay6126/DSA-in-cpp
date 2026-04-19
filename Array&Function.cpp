c268bea5aa9f09349fbd982751de6cddd3863554
Ranjay6126 <6126ranjayprajapati@gmail.com>
2026-04-19 18:01:32 +0530

    add the array with function

--- Array&Function.cpp
+++ Array&Function.cpp
@@ -1,29 +1,51 @@
-#include<iostream>          // Includes input-output stream library
-using namespace std;        // Allows use of cout, cin without std::
+// #include<iostream>          // Includes input-output stream library
+// using namespace std;        // Allows use of cout, cin without std::
 
-// Function to increment first element of array
-void inc (int arr[], int size){
-     arr[0] += 10;         // Adds 10 to first element of array
-}
+// // Function to increment first element of array
+// void inc (int arr[], int size){
+//      arr[0] += 10;         // Adds 10 to first element of array
+// }
 
-// Function to print array elements
-void printinc(int arr[], int size){
-    for(int i=0; i<size; i++){   // Loop from index 0 to size-1
-        cout << arr[i] << " ";   // Print each element with space
+// // Function to print array elements
+// void printinc(int arr[], int size){
+//     for(int i=0; i<size; i++){   // Loop from index 0 to size-1
+//         cout << arr[i] << " ";   // Print each element with space
+//     }
+//     cout << endl;                // Move to next line after printing array
+// } 
+
+// int main(){
+    
+//     int arr[] = {1,3,5};   // Declare and initialize array
+//     int size = 3;          // Store size of array
+    
+//     inc(arr, size);        // Call function to modify array (arr[0] becomes 11)
+//     printinc(arr, size);   // Print updated array → 11 3 5
+    
+//     for(int i=0; i<size; i++){  // Loop to print array again
+//         cout << arr[i]<< " ";       // Print elements without space
+//     }
+    
+// }
+
+
+//--------------------------------------------------//
+
+#include<iostream>
+using namespace std;
+
+void printingArray(int third[], int size){
+    for (int i=0; i<size; i++){
+        cout<<third[i];
     }
-    cout << endl;                // Move to next line after printing array
-} 
+    cout<<"printing array done"<<endl;
+}
 
 int main(){
+    int third[]={2,3,5};
+    int size = 3;
     
-    int arr[] = {1,3,5};   // Declare and initialize array
-    int size = 3;          // Store size of array
-    
-    inc(arr, size);        // Call function to modify array (arr[0] becomes 11)
-    printinc(arr, size);   // Print updated array → 11 3 5
-    
-    for(int i=0; i<size; i++){  // Loop to print array again
-        cout << arr[i]<< " ";       // Print elements without space
-    }
+    printingArray(third, size);
     
 }
+

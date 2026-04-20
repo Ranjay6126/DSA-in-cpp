447fdae212a7e55d4591284641bd5c395e5f857c
Ranjay6126 <6126ranjayprajapati@gmail.com>
2026-04-20 22:16:47 +0530

    Kth smallest in sorted Matrix

---
+++ Kth Smallest in Sorted Matrix (Heap).cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int kthSmallest(vector<vector<int>>& matrix, int k) {
+    priority_queue<int> pq;
+
+    for(auto &row : matrix){
+        for(int x : row){
+            pq.push(x);
+            if(pq.size() > k) pq.pop();
+        }
+    }
+    return pq.top();
+}

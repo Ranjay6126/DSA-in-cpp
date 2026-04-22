ff8cebb2521bddb7f46080c1a0939ed05db96e2e
Ranjay6126 <6126ranjayprajapati@gmail.com>
2026-04-22 17:12:26 +0530

    linear search

---
+++ Array/Linear_SearchArray.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+
+int Linear_search(int arr[], int size, int key){
+    for(int i = 0; i < size; i++){
+        if(arr[i] == key){
+            return i;
+        }
+    }
+    return -1;
+}
+
+int main(){
+
+    int arr[100];
+    int size;
+
+    cout << "Enter the size of array: ";
+    cin >> size;
+
+    cout << "Enter the elements of the array:" << endl;
+    for(int i = 0; i < size; i++){
+        cin >> arr[i];
+    }
+
+    int key;
+    cout << "Enter the element you want to search: ";
+    cin >> key;
+
+    int result = Linear_search(arr, size, key);
+
+    if(result != -1){
+        cout << "Key found at index: " << result << endl;
+    } 
+    else{
+        cout << "Key does not exist in the array" << endl;
+    }
+
+    return 0; 
+}

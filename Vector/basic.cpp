1cd9bba48352fa7913cbb22aef9e18b5a9cb1ec5
Ranjay6126 <6126ranjayprajapati@gmail.com>
2026-04-24 18:21:16 +0530

    vector

--- Vector/basic.cpp
+++ Vector/basic.cpp
@@ -19,4 +19,19 @@ int main(){
 
       cout<<arr[1]<<endl;
 
+      // vector<int>brr(10);
+      //  vector<int>brr(10,-1);
+      // vecotr<int> crr{10,20,30};
+      int n;
+      cout<<"Enter the value of n"<<endl;
+      cin>>n;
+      vector<int>brr(n,-101);
+      cout<<"size of b "<< brr.size()<<endl;
+      cout<<"capacity of b "<<brr.capacity()<<endl;
+
+      for(int i=0; i<brr.size(); i++){
+        cout<<brr[i]<< " ";
+      }
+      cout<< endl;
+      return 0;
     }

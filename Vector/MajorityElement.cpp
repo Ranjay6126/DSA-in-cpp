937a180caeb5fb85b18e083ff0901af179dc2673
Ranjay6126 <6126ranjayprajapati@gmail.com>
2026-04-30 23:51:38 +0530

    majority element

---
+++ Vector/MajorityElement.cpp
@@ -0,0 +1,16 @@
+int majorityElement(vector<int>& nums) {
+    int candidate = 0, count = 0;
+
+    for (int i = 0; i < nums.size(); i++) {
+        if (count == 0) {
+            candidate = nums[i];
+        }
+
+        if (nums[i] == candidate)
+            count++;
+        else
+            count--;
+    }
+
+    return candidate;
+}

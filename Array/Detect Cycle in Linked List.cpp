9feb46c99ba69bc2ac6ca91b62fc9d3c75866d3e
Ranjay6126 <6126ranjayprajapati@gmail.com>
2026-05-27 21:47:07 +0530

    detect cycle in linked list

---
+++ Array/Detect Cycle in Linked List.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+using namespace std;
+
+class Node{
+public:
+    int data;
+    Node* next;
+
+    Node(int val){
+        data = val;
+        next = NULL;
+    }
+};
+
+bool hasCycle(Node* head){
+
+    Node* slow = head;
+    Node* fast = head;
+
+    while(fast != NULL && fast->next != NULL){
+
+        slow = slow->next;
+        fast = fast->next->next;
+
+        if(slow == fast){
+            return true;
+        }
+    }
+
+    return false;
+}
+
+int main(){
+
+    Node* head = new Node(1);
+    head->next = new Node(2);
+    head->next->next = new Node(3);
+    head->next->next->next = head;
+
+    if(hasCycle(head)){
+        cout << "Cycle Detected";
+    }
+    else{
+        cout << "No Cycle";
+    }
+
+    return 0;
+}

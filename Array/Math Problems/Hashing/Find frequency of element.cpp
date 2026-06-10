5c2e32e610ccc4568b7d30df3c642e427d85423f
Ranjay6126 <6126ranjayprajapati@gmail.com>
2026-06-10 22:47:12 +0530

    frequency of element

---
+++ Array/Math Problems/Hashing/Find frequency of element.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <unordered_map>
+using namespace std;
+
+int main() {
+    int arr[] = {1,2,1,3,2,1};
+    int n = 6;
+
+    unordered_map<int,int> freq;
+
+    for(int i=0;i<n;i++)
+        freq[arr[i]]++;
+
+    for(auto x : freq)
+        cout << x.first << " -> " << x.second << endl;
+
+    return 0;
+}

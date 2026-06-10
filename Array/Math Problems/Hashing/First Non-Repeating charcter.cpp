7fadc41fd7141e89e33e458efed8e085523ddc0c
Ranjay6126 <6126ranjayprajapati@gmail.com>
2026-06-10 22:48:21 +0530

    first non-repeating character

---
+++ Array/Math Problems/Hashing/First Non-Repeating charcter.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <unordered_map>
+using namespace std;
+
+int main() {
+    string s = "aabbcde";
+
+    unordered_map<char,int> freq;
+
+    for(char ch : s)
+        freq[ch]++;
+
+    for(char ch : s) {
+        if(freq[ch] == 1) {
+            cout << ch;
+            break;
+        }
+    }
+
+    return 0;
+}

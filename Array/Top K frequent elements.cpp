0c6d8c16d300c040e499e92c13213bd7dc1c754b
Ranjay6126 <6126ranjayprajapati@gmail.com>
2026-06-15 23:28:53 +0530

    top k frequent element

---
+++ Array/Top K frequent elements.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <queue>
+using namespace std;
+
+vector<int> topKFrequent(vector<int>& nums, int k) {
+
+    unordered_map<int,int> freq;
+
+    for(int x : nums)
+        freq[x]++;
+
+    priority_queue<pair<int,int>> pq;
+
+    for(auto p : freq)
+        pq.push({p.second,p.first});
+
+    vector<int> ans;
+
+    while(k--) {
+        ans.push_back(pq.top().second);
+        pq.pop();
+    }
+
+    return ans;
+}

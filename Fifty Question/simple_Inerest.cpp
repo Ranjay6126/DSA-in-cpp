4477cabf2195ddc442c066a0fcf5344bd72d1822
Ranjay6126 <6126ranjayprajapati@gmail.com>
2026-06-17 19:09:43 +0530

    target 50 question

---
+++ Fifty Question/simple_Inerest.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double p = 100;
+    double r = 4.2;
+    double t = 2;
+
+    double simple_interest = (p * r * t) / 100;
+
+    cout << "Simple Interest = " << simple_interest << endl;
+
+    return 0;
+}

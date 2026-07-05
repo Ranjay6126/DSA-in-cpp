958d7b7ecf4ef38e26b60be9e4160e7026591796
Ranjay6126 <6126ranjayprajapati@gmail.com>
2026-07-05 23:51:39 +0530

    Kth roated no roated

--- kth roated no.cpp
+++ kth roated no.cpp
@@ -1,33 +1,32 @@
 class Solution {
-public:
+  public:
     int findKRotation(vector<int> &arr) {
-
+        // Code Here
         int n = arr.size();
         int start = 0;
-        int end = n - 1;
-
-        while (start <= end) {
-
-            // If the current part is already sorted
-            if (arr[start] <= arr[end])
-                return start;
-
-            int mid = start + (end - start) / 2;
-
-            int next = (mid + 1) % n;
-            int prev = (mid + n - 1) % n;
-
-            // Minimum element found
-            if (arr[mid] <= arr[next] && arr[mid] <= arr[prev])
+        int end = n-1;
+        
+        while(start <= end){
+            
+            int mid = start + (end - start)/2;
+            
+            int next = arr[(mid+1) % n];
+            int prev = arr[(mid + n -1) % n];
+            
+            
+            if(arr[mid] <= next && arr[mid] <= prev){
                 return mid;
-
-            // Left half is sorted
-            if (arr[start] <= arr[mid])
+                
+            } 
+            else if(arr[start] <= arr[mid]){
+                
                 start = mid + 1;
-            else
+                
+            } 
+            else{
                 end = mid - 1;
+            }
         }
-
         return 0;
     }
 };

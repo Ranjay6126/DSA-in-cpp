b6aaa21a0c9d3ae2b09c1becad6296e3fa9a6897
Ranjay6126 <6126ranjayprajapati@gmail.com>
2026-09-19 10:44:20 +0530

    gcd

---
+++ Array/Math Problems/gcd_euclid.cpp
@@ -0,0 +1,24 @@
+//GCD of two Number
+
+#include<iostream>
+using namespace std;
+
+int gcd(int a, int b){
+    while(a>0 && b>0){
+          if(a>b){
+          a = a%b;   
+    } else{
+      b = b%a;
+    } 
+    }
+    if(a == 0)return b;
+    return a;
+    
+}
+int main(){
+    int a = 20;
+    int b = 28;
+
+    int ans = gcd(a, b);
+    cout<<ans;
+}

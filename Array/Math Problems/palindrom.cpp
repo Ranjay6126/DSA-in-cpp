3bff9bf095106a0dbfe6e732d82cfb3ea1bb9804
Ranjay6126 <6126ranjayprajapati@gmail.com>
2026-09-19 17:07:55 +0530

    optimized solution for palindrom

---
+++ Array/Math Problems/palindrom.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+
+    if(x < 0){
+        return false;
+    }
+        int rev = 0;
+         int original = x;
+
+        while( x != 0){
+            int digit = x%10;
+
+            if(rev > INT_MAX/10 || rev < INT_MIN/10){
+                return 0;
+            }
+          
+          rev = (rev * 10)+digit;
+
+          x = x/10;
+
+        }
+        if(rev == original){
+            return true;
+        }
+        return false;
+      
+    }
+};
